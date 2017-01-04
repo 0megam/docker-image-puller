6e1882857470746991c0c7a9d958f20acab40201
root <root@nodejs.mirror.docker>
2017-01-04 10:12:19 +0200

    new file:   containers.txt 	new file:   puller.sh

---
+++ puller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for image in $(cat containers.txt); do
+	docker pull $image
+	archive=${image////%}
+	archive=${archive/:/@}
+	if [ ! -f ./images/$archive.tar.gz ]; then
+	echo Start saving image to ./images/$archive.tar.gz
+	docker save $image > ./images/$archive.tar.gz
+        echo Done.
+        fi
+done
